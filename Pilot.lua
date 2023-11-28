3534be8e24141a4471dc60d5d72b45c504dd531e
vma <106908003+0vma@users.noreply.github.com>
2023-11-28 20:16:33 +0100

    Create Pilot.lua

---
+++ Pilot.lua
@@ -0,0 +1,13 @@
+return {
+  ScriptStates = {
+      Status = {
+          ["ClickSimulator"] = {"0.0.1 BETA", 1701198863924, true, "downtime"}
+      }
+			DisallowedStates = {
+					"XK", -- not recognised by developers state
+					"IS", -- till the end of the current conflict (palestine israel)
+					"UA" -- majorly unsupported by developers state
+			};
+      
+	},
+}  
