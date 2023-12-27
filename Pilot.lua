136fec1fd2244fd3e461600c6ea3db04646e98f2
vma <106908003+0vma@users.noreply.github.com>
2023-12-27 01:23:04 +0100

    Update Pilot.lua

--- Pilot.lua
+++ Pilot.lua
@@ -1,13 +1,13 @@
 return {
 	ScriptStates = {
-      Status = {
-          [8737899170] = {"0.0.1 BETA", false, "downtime"}
-      }
-			DisallowedStates = {
+	      Status = {
+	          [8737899170] = {"0.0.1 BETA", false, "downtime"}
+	      },
+	      DisallowedStates = {
 							"XK", -- not recognised by developers state
 							"IS", -- till the end of the current conflict (palestine israel)
 							"UA" -- majorly unsupported by developers state
-			};
+	      };
       
 	},
 }  
